964eebd3dd4c03b02370b37c38fa8761ed191262
Santiago-Serrano2004 <santisgoserranoortiz@gmail.com>
2025-11-09 03:59:56 -0500

    47

--- app.py
+++ app.py
@@ -1301,7 +1301,7 @@ elif pagina == "📊 Reporte General":
             marker_colors=['#27ae60', '#3498db', '#f39c12', '#e67e22', '#e74c3c']
         )])
         fig.update_traces(textposition='inside', textinfo='percent+label')
-        fig.update_layout(height=800, width= 800, title="Distribución del Rendimiento")
+        fig.update_layout(height=600, width= 600, title="Distribución del Rendimiento")
         st.plotly_chart(fig, use_container_width=True)
     
     st.markdown("---")
