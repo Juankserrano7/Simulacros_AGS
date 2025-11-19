6911b3ab7cffbb6940ef73e56dd974c47d30c8a3
Santiago-Serrano2004 <santisgoserranoortiz@gmail.com>
2025-11-19 00:19:31 -0500

    103

--- app.py
+++ app.py
@@ -660,7 +660,6 @@ if not st.session_state.authenticated:
     with col_login:
 
         with st.form("login_profesores"):
-            st.markdown("<div class='login-background', style = color: #FFFFFF;>", unsafe_allow_html=True)
             email_input = st.text_input(
                 "📧 Correo institucional",
                 placeholder="nombre.apellido@aspaen.edu.co",
@@ -681,8 +680,7 @@ if not st.session_state.authenticated:
                 use_container_width=True,
                 type="primary"
             )
-            st.markdown("</div>", unsafe_allow_html=True)
-
+        
         if login:
             if verificar_credenciales(email_input, password_input, usuarios_auth):
                 st.session_state.authenticated = True
