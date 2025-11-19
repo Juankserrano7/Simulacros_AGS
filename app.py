15b08578a0f6969982b2f251c67719b686d6d40a
Santiago-Serrano2004 <santisgoserranoortiz@gmail.com>
2025-11-18 20:59:29 -0500

    69

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 
+
 # Configuración de la página
 st.set_page_config(
     page_title="Dashboard Simulacros PreIcfes",
